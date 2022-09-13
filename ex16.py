ac6e70434448d19d0e1e3f30cac3bb86a2f1ef4d
Bahaa <qgunbb21@gmail.com>
2022-09-13 18:50:01 +0200

    first commit

---
+++ ex16.py
@@ -0,0 +1,25 @@
+# # task1
+# import re
+
+# text_search = "Berlin is a world city of culture, politics, media and science."
+# pattern = r"(\s+)"
+# match =re.search(pattern,text_search)
+# print(match)
+
+# #task 2
+# import re
+
+# text = "Berlin is a world city of culture, politics, media and science."
+# pattern =r"(Frankfurt)"
+# match = re.search(pattern,text)
+# print(match)
+
+# #task3
+
+# import re
+# text ="Berlin is a city of culture."
+# match = re.sub('\s','-', text)
+# print(match)
+
+#task 4
+
